ce2ae9c84b6c64a157919c894fc3b886ac1d3be9
Reza Hedayati <gunner_1369@yahoo.com>
2019-06-22 22:51:19 +0430

    Exercise 1. Servelet with variables in URL

---
+++ Ex1/src/main/webapp/ir/maktab/MathServlet.java
@@ -0,0 +1,45 @@
+package main.webapp.ir.maktab;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MathServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+
+        out.println("<title>MyMathPage</title>");
+        out.println("</head>");
+        out.println("<body>");
+
+        ServletConfig servletConfig = getServletConfig();
+        String value = servletConfig.getInitParameter("operationName");
+
+        Double num1 = Double.parseDouble(req.getParameter("num1"));
+        Double num2 = Double.parseDouble(req.getParameter("num2"));
+
+        if (value.equals("add")) {
+            out.println("<h1> " + num1 + " + " + num2 + " = " + (num1+num2) + " </h1>");
+        }
+        else if (value.equals("minus")) {
+            out.println("<h1> " + num1 + " - " + num2 + " = " + (num1-num2) + " </h1>");
+        }
+        else if (value.equals("product")) {
+            out.println("<h1> " + num1 + " * " + num2 + " = " + (num1*num2) + " </h1>");
+        }
+        else if (value.equals("division")) {
+            out.println("<h1> " + num1 + " / " + num2 + " = " + (num1/num2) + " </h1>");
+        }
+
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
